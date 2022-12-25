d4546fd5c508eba515930136d47751ea62173de8
Chiranjeev-droid <somu209e@gmail.com>
2022-12-25 23:08:56 +0530

    modals done

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+    unique: [true, "Please enter unique email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a strong password"],
+    minLength: [6, "Password must be 6 digits long"],
+    select: false,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+module.exports = mongoose.model("User", userSchema);
