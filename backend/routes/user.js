c20d776c0f1ee9161bbb54380903f2ebe6c06e07
Chiranjeev-droid <somu209e@gmail.com>
2022-12-27 11:57:10 +0530

    logout route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { register, login, followUser } = require("../controllers/user");
+const { register, login, followUser, logout } = require("../controllers/user");
 const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
 router.route("/follow/:id").get(isAuthenticated, followUser);
 module.exports = router;
